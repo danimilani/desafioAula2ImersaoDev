b3b69c7948069493701a27fa315d60c0b739451e
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2021-03-28 17:25:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+//var primeiroValor = parseInt(prompt("Digite o primeiro número:"))
+//var segundoValor = parseInt(prompt("Digite o segundo número:"))
+//var operacao = prompt("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração")
+
+function test1(){
+    var chuteAnimalOne = prompt("What is the name of this animal?")
+    console.log(chuteAnimalOne)
+    if(chuteAnimalOne == "monkey"){
+        alert("Correct!")
+    } else {
+        alert("Incorrect!")
+    }
+}
+
+function test2(){
+    var chuteAnimalTwo = prompt("What is the name of this animal?")
+    console.log(chuteAnimalTwo)
+    if(chuteAnimalTwo == "cat"){
+        alert("Correct!")
+    } else {
+        alert("Incorrect!")
+    }
+}
+
+function test3(){
+    var chuteAnimalThree = prompt("What is the name of this animal?")
+    console.log(chuteAnimalThree)
+    if(chuteAnimalThree == "dog"){
+        alert("Correct!")
+    } else {
+        alert("Incorrect!")
+    }
+}
+
+
+
+
+
+
+//if - se
+//else - senão
+//else if - senão se
